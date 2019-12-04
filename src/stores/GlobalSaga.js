041497409765cbe85c85cb98ce4df35f517092b6
Vitor A Thomazini <vitor.thomazini94@gmail.com>
2019-12-03 23:37:43 -0300

    Refactory

--- src/stores/GlobalSaga.js
+++ src/stores/GlobalSaga.js
@@ -1,9 +1,7 @@
-import { takeLatest, cancel, delay, put } from "redux-saga/effects";
+import { takeLatest, put } from "redux-saga/effects";
 import { setTokenAction } from "./GlobalProvider";
 
 const LOGIN = "LOGIN_GLOBAL_SAGA";
-const WAIT_USER_ACTION = "WAIT_USER_ACTION_GLOBAL_SAGA";
-const USING_SYSTEM = "USING_SYSTEM_GLOBAL_SAGA";
 const LOGOUT = "LOGOUT_GLOBAL_SAGA";
 
 export const loginAction = () => ({
@@ -15,36 +13,15 @@ export const logoutAction = () => ({
   type: LOGOUT
 });
 
-export const waitUserAction = () => ({
-  type: WAIT_USER_ACTION,
-  time: 30000
-});
-
-export const usingSystemAction = () => ({
-  type: USING_SYSTEM
-});
-
 function* handleLogin(actions) {
   yield put(setTokenAction(actions.token));
 }
 
-function* handleLogout(actions) {
+function* handleLogout() {
   yield put(setTokenAction(null));
-}
-
-function* handleWaitUser(actions) {
-  yield delay(actions.time);
-  yield put(logoutAction());
-}
-
-function* handleUsingSystem(task) {
-  yield cancel(task);
-  yield put(usingSystemAction());
 }
 
 export default function* watchGlobal() {
   yield takeLatest(LOGIN, handleLogin);
   yield takeLatest(LOGOUT, handleLogout);
-  const waitUser = yield takeLatest(WAIT_USER_ACTION, handleWaitUser);
-  yield takeLatest(USING_SYSTEM, handleUsingSystem, waitUser);
 }
